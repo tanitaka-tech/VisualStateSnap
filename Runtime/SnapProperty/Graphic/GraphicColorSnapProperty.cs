dc2c340d1844d4de9da816427316bd23ef27c2c3
tanitaka_tech <78785830+tanitaka-tech@users.noreply.github.com>
2023-11-19 03:42:16 +0900

    🚀️ UIColorをSnapPropertyに追加

---
+++ Runtime/SnapProperty/Graphic/GraphicColorSnapProperty.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace TanitakaTech.VisualStateSnap.SnapProperty.Graphic
+{
+    [Serializable]
+    public class GraphicColorSnapProperty : SnapProperty<UnityEngine.UI.Graphic, Color>
+    {
+        public override string CustomName => "color";
+        
+        public GraphicColorSnapProperty(UnityEngine.UI.Graphic snapTargetReference) : base(snapTargetReference)
+        {
+        }
+
+        protected override void ApplySnappedValue()
+        {
+            if (SnapTargetReference != null)
+            {
+                SnapTargetReference.color = SnappedValue;
+            }
+        }
+
+        protected override Color GetCurrentValue()
+        {
+            return SnapTargetReference!?.color ?? Color.white;
+        }
+    }
+    
+    [Serializable]
+    public class GraphicColorSnapTargetReference : SnapTargetReference<UnityEngine.UI.Graphic, GraphicColorSnapProperty>
+    {
+        protected override GraphicColorSnapProperty CreateSnapProperty() => new (_snapTargetReference);
+    }
+}
